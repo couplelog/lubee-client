d96a2a602613c76a09ba08c82ff57726797cb6eb
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-04 13:18:58 +0900

    etc: 아이콘, 이미지

--- lubee/src/assets/index.ts
+++ lubee/src/assets/index.ts
@@ -1,4 +1,31 @@
 import LogoIc from "./icon/logo.svg?react";
-// import DownArrowIc from "./icon/downArrow.svg?react";
+import BackIc from "./icon/backIc.svg?react";
+import LocationPointIc from "./icon/locationPointIc.svg?react";
+import TrashIc from "./icon/trashIc.svg?react";
+import YellowHeartIc from "./icon/yellowHeartIc.svg?react";
+import MintHeartIc from "./icon/mintHeartIc.svg?react";
+import YellowBangIc from "./icon/yellowBangIc.svg?react";
+import MintBangIc from "./icon/mintBangIc.svg?react";
+import YellowThumIc from "./icon/yellowThumbIc.svg?react";
+import MintThumIc from "./icon/mintThumIc.svg?react";
+import YellowSmileIc from "./icon/yellowSmileIc.svg?react";
+import MintSmileIc from "./icon/mintSmileIc.svg?react";
+import YellowHoneyIc from "./icon/yellowHoneyIc.svg?react";
+import MintHoneyIc from "./icon/mintHoneyIc.svg?react";
 
-export { LogoIc };
+export {
+  LogoIc,
+  BackIc,
+  LocationPointIc,
+  TrashIc,
+  YellowHeartIc,
+  MintHeartIc,
+  YellowBangIc,
+  MintBangIc,
+  YellowThumIc,
+  MintThumIc,
+  YellowSmileIc,
+  MintSmileIc,
+  YellowHoneyIc,
+  MintHoneyIc,
+};
---
+++ lubee/src/styles/@common/commonEmoji.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import {
+  YellowHeartIc,
+  YellowBangIc,
+  YellowHoneyIc,
+  YellowSmileIc,
+  YellowThumIc,
+  MintHeartIc,
+  MintBangIc,
+  MintHoneyIc,
+  MintSmileIc,
+  MintThumIc,
+} from "@assets/index";
+
+export const YellowHeartIcon = styled(YellowHeartIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const YellowBangIcon = styled(YellowBangIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const YellowHoneyIcon = styled(YellowHoneyIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const YellowSmileIcon = styled(YellowSmileIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const YellowThumIcon = styled(YellowThumIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const MintHeartIcon = styled(MintHeartIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const MintBangIcon = styled(MintBangIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const MintHoneyIcon = styled(MintHoneyIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const MintSmileIcon = styled(MintSmileIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
+
+export const MintThumIcon = styled(MintThumIc)`
+  width: 3.48rem;
+  height: 3.48rem;
+`;
