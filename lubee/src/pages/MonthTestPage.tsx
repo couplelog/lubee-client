c2354137d7876072e7bc9a029c794642d41c9000
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-06 19:49:49 +0900

    design: 이번달 페이지

---
+++ lubee/src/pages/MonthTestPage.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { useState, useRef, useEffect } from "react";
+import Header from "@components/fullPicToday/Header";
+import DeletePicModal from "@components/fullPicToday/DeletePicModal";
+import EmojiDetailModal from "@components/fullPicToday/EmojiDetailModal";
+import MonthPic from "@components/fullPicMonth/MonthPic";
+
+export default function MonthTestPage() {
+  const [openEmojiDetail, setOpenEmojiDetail] = useState<boolean>(false);
+  const [openDeletePicModal, setOpenDeletePicModal] = useState<boolean>(false);
+  const [selectedEmojiText, setSelectedEmojiText] = useState<string>(localStorage.getItem("emoji") || "");
+
+  function handleTrashBtn(open: boolean) {
+    setOpenDeletePicModal(open);
+  }
+
+  /*모달 애니메이션*/
+  const modalRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (!modalRef.current || modalRef.current.contains(e.target as Node)) return;
+      setOpenEmojiDetail(false);
+    };
+    document.addEventListener("mousedown", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [modalRef]);
+
+  return (
+    <Wrapper>
+      <Header handleTrashBtn={handleTrashBtn} />
+      <MonthPic
+        setOpenEmojiDetail={setOpenEmojiDetail}
+        setSelectedEmojiText={setSelectedEmojiText}
+        selectedEmojiText={selectedEmojiText}
+      />
+      {openDeletePicModal && <DeletePicModal handleTrashBtn={handleTrashBtn} />}
+      {openEmojiDetail && <EmojiDetailModal ref={modalRef} selectedEmojiText={selectedEmojiText} />}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
