93d0e2f8ba7e49c2b2646046a2bee7d3fcfa7068
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-08 15:57:07 +0900

    etc: root 공통폴더 파일들

---
+++ lubee/src/@common/components/FullPicContainer.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { fullPicStyle } from "@styles/picStyle";
+import Location from "./LocationTag";
+
+interface FullPicProps {
+  picSrc: string;
+  location: string;
+  setOpenLocationModal?: (open: boolean) => void;
+}
+
+// React.Dispatch<React.SetStateAction<boolean>>
+
+export default function FullPicContainer(props: FullPicProps) {
+  const { picSrc, location, setOpenLocationModal } = props;
+
+  return (
+    <PicBox>
+      <Pic src={picSrc}></Pic>
+      <Location location={location} setOpenLocationModal={setOpenLocationModal} />
+    </PicBox>
+  );
+}
+
+const PicBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  align-items: center;
+  width: 100%;
+  padding: 1.8rem 2rem 0;
+`;
+
+const Pic = styled.img`
+  ${fullPicStyle};
+`;
---
+++ lubee/src/@common/core/locationData.ts
@@ -0,0 +1,78 @@
+interface LocationDataProps {
+  name: string;
+  distance: number;
+  info: string;
+}
+
+export const locationData: LocationDataProps[] = [
+  {
+    name: "하이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "어디까지 길어지나 테스트",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "짧은",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "등심",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "고",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "g",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "hellollllllllllllllllllllllllllllllll",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+  {
+    name: "심플리타이",
+    distance: 56,
+    info: "서울특별시 마포구 동교로 248-2",
+  },
+];
