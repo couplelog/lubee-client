4623ea95d9b13d177aff1322971e18a4f8032c5b
안혜연 <ahy020606@naver.com>
2024-07-08 18:09:57 +0900

    etc: 프로필 아이콘 추가

--- lubee/src/assets/index.ts
+++ lubee/src/assets/index.ts
@@ -22,6 +22,18 @@ import SymbolIc from "./icon/symbolIc.svg?react";
 import SymbolLoginIc from "./icon/symbolLoginIc.svg?react";
 import LubeeCodeIc from "./icon/lubeeCodeIc.svg?react";
 import CopyIc from "./icon/copyIc.svg?react";
+import Profile1Ic from "./icon/profile1Ic.svg?react";
+import Profile2Ic from "./icon/profile2Ic.svg?react";
+import Profile3Ic from "./icon/profile3Ic.svg?react";
+import Profile4Ic from "./icon/profile4Ic.svg?react";
+import Profile5Ic from "./icon/profile5Ic.svg?react";
+import Profile6Ic from "./icon/profile6Ic.svg?react";
+import ProfileStroke1Ic from "./icon/profileStroke1Ic.svg?react";
+import ProfileStroke2Ic from "./icon/profileStroke2Ic.svg?react";
+import ProfileStroke3Ic from "./icon/profileStroke3Ic.svg?react";
+import ProfileStroke4Ic from "./icon/profileStroke4Ic.svg?react";
+import ProfileStroke5Ic from "./icon/profileStroke5Ic.svg?react";
+import ProfileStroke6Ic from "./icon/profileStroke6Ic.svg?react";
 
 export {
   LogoIc,
@@ -48,4 +60,16 @@ export {
   SymbolLoginIc,
   LubeeCodeIc,
   CopyIc,
+  Profile1Ic,
+  Profile2Ic,
+  Profile3Ic,
+  Profile4Ic,
+  Profile5Ic,
+  Profile6Ic,
+  ProfileStroke1Ic,
+  ProfileStroke2Ic,
+  ProfileStroke3Ic,
+  ProfileStroke4Ic,
+  ProfileStroke5Ic,
+  ProfileStroke6Ic,
 };
--- lubee/src/pages/Onboarding/OnboardingProfilePage.tsx
+++ lubee/src/pages/Onboarding/OnboardingProfilePage.tsx
@@ -1,3 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Header from "@components/onboarding/Header";
+import TitleBox from "@components/onboarding/TitleBox";
+import NumberBox from "@components/onboarding/NumberBox";
+import OnboardingBtn from "@components/onboarding/OnboardingBtn";
+
 export default function OnboardingProfilePage() {
-  return <div>OnboardingProfilePage</div>;
+  const navigate = useNavigate();
+  const [nickname, setNickname] = useState("");
+
+  const isOnboardingBtnDisabled = nickname === "";
+
+  function handleBackBtn() {
+    navigate("/Onboarding");
+  }
+
+  function handleXBtn() {
+    navigate("/Login");
+  }
+
+  function handleOnboardingBtn() {
+    navigate("/OnboardingProfileBirth");
+  }
+
+  return (
+    <Wrapper>
+      <Header handleBackBtn={handleBackBtn} handleXBtn={handleXBtn} showBackIcon showXIcon />
+      <TitleBox titleText="연인의 러비코드를 입력하세요" subtitleText="러비에서 쓰일 애칭이에요" />
+      <NumberBox inputValue={nickname} setInputValue={setNickname} $disabled={true} placeholder="닉네임 입력" />
+      <OnboardingBtn handleOnboardingBtn={handleOnboardingBtn} text="연결하기" $disabled={isOnboardingBtnDisabled} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
