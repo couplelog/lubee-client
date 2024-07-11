e8f855f66bac51d1dcb07cb2f92233e6e4f5ef6b
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-11 17:34:24 +0900

    design: 오늘 페이지 blankImgBtn 틀만

--- lubee/src/home/today/index.tsx
+++ lubee/src/home/today/index.tsx
@@ -1,3 +1,32 @@
+import styled from "styled-components";
+import { imagesData } from "@common/core/imagesData";
+import blankImg from "@assets/image/blankImg.png";
+import BlankImgBtn from "home/components/BlankImgBtn";
+
 export default function index() {
-  return <div>오늘 페이지</div>;
+  /*이미지 개수가 5개 이하이면 이미지 추가하는 버튼 만들어주는 array*/
+  const displayPics =
+    imagesData.length < 5 ? [...imagesData.map((img) => img.imgSrc), blankImg] : imagesData.map((img) => img.imgSrc);
+
+  return (
+    <PicBox>
+      {displayPics.map((imgSrc, index) =>
+        imgSrc === blankImg ? <BlankImgBtn index={index} /> : <Image key={index} src={imgSrc} />,
+      )}
+    </PicBox>
+  );
 }
+
+const PicBox = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.6rem;
+  overflow: scroll;
+  scrollbar-width: none;
+`;
+
+const Image = styled.img`
+  padding: 0;
+  border: none;
+  background: none;
+`;
