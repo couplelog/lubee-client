4414e3b4be39e4ee9199e859ca689c258e07e760
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-15 14:33:23 +0900

    fix: dirname 배포에러

--- lubee/vite.config.ts
+++ lubee/vite.config.ts
@@ -8,18 +8,6 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "src") },
-      { find: "@api", replacement: path.resolve(__dirname, "src/api") },
-      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
-      {
-        find: "@components",
-        replacement: path.resolve(__dirname, "src/components"),
-      },
-      { find: "@core", replacement: path.resolve(__dirname, "src/core") },
-      { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
-      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
-      { find: "@types", replacement: path.resolve(__dirname, "src/types") },
-    ],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
