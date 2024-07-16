5ddb697b7685cbb55a1024db9cb1094aafd8502a
안혜연 <ahy020606@naver.com>
2024-07-16 21:30:01 +0900

    etc: 네이밍 변경

---
+++ lubee/src/mypage/components/MypageHeader.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export default function MypageHeader() {
+  return <div>MypageHeader</div>;
+}
--- lubee/src/mypage/main/index.tsx
+++ lubee/src/mypage/main/index.tsx
@@ -1,5 +1,30 @@
 import styled from "styled-components";
+import { SettingIc, EditIc, PlayIc, HoneyYellowIc, BannerIc } from "@assets/index";
 
 export default function index() {
   return <div>index</div>;
 }
+
+const SettingIcon = styled(SettingIc)`
+  width: 2.4rem;
+  height: 2.4rem;
+`;
+
+const EditIcon = styled(EditIc)`
+  width: 1.6rem;
+  height: 1.6rem;
+`;
+
+const PlayIcon = styled(PlayIc)`
+  width: 1.6rem;
+  height: 1.6rem;
+`;
+const HoneyYellowIcon = styled(HoneyYellowIc)`
+  width: 1.6rem;
+  height: 1.6rem;
+`;
+
+const BannerIcon = styled(BannerIc)`
+  width: 16.4699rem;
+  height: 11.852rem;
+`;
