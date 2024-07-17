0dd1cabc960a0c931b696f7cf359ad8bad6e4af9
안혜연 <ahy020606@naver.com>
2024-07-17 19:58:34 +0900

    design: 마이페이지 프로그래스바

--- lubee/src/mypage/components/HoneyBox.tsx
+++ lubee/src/mypage/components/HoneyBox.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { HoneyYellowIc, PlayIc } from "@assets/index";
+import { HoneyYellowIc, PlayIc, RewindIc } from "@assets/index";
 
 interface HoneyBoxProps {
   count: number;
@@ -7,23 +7,44 @@ interface HoneyBoxProps {
 
 export default function HoneyBox(props: HoneyBoxProps) {
   const { count } = props;
+  const total = 50;
+  const percentage = (count / total) * 100;
 
   return (
     <Container>
-      <TitleContainer>
-        <HoneyYellowIcon />
-        <TitleText>전체 꿀 {count}개</TitleText>
-        <SubtitleText>리와인드까지 25개</SubtitleText>
-      </TitleContainer>
-      <PlayIcon />
-      <CountText $count={false}>50</CountText>
+      <HoneyContainer>
+        <TitleContainer>
+          <HoneyYellowIcon />
+          <TitleText>전체 꿀 {count}개</TitleText>
+          <SubtitleText>리와인드까지 25개</SubtitleText>
+        </TitleContainer>
+        <ProgressContainer>
+          <ProgressBarContainer>
+            <ProgressBar style={{ width: `${percentage}%` }} />
+          </ProgressBarContainer>
+          <PlayIcon />
+          <TextContainer>
+            <CountText $count={true} $percentage={percentage}>
+              {count}
+            </CountText>
+            <CountText $count={false}>50</CountText>
+          </TextContainer>
+        </ProgressContainer>
+      </HoneyContainer>
+      <RewindIcon />
     </Container>
   );
 }
 
 const Container = styled.section`
   display: flex;
+  gap: 0.882rem;
+`;
+
+const HoneyContainer = styled.section`
+  display: flex;
   flex-direction: column;
+  gap: 0.8rem;
   width: 100%;
   height: 7.6rem;
   padding: 1.1rem 1.3rem 0.5rem 1.2rem;
@@ -56,13 +77,52 @@ const SubtitleText = styled.p`
   color: ${({ theme }) => theme.colors.gray_500};
 `;
 
+const ProgressContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+  position: relative;
+`;
+
+const ProgressBarContainer = styled.div`
+  width: 23.3rem;
+  height: 0.8rem;
+  border-radius: 32px;
+  background-color: ${({ theme }) => theme.colors.gray_100};
+`;
+
+const ProgressBar = styled.div`
+  height: 0.8rem;
+  border-radius: 32px;
+  background-color: ${({ theme }) => theme.colors.yellow};
+`;
+
 const PlayIcon = styled(PlayIc)`
+  position: absolute;
+  top: 10%;
+  right: 0;
+  transform: translate(-50%, -50%);
   width: 1.6rem;
   height: 1.6rem;
 `;
 
-const CountText = styled.p<{ $count: boolean }>`
+const TextContainer = styled.section`
+  display: flex;
+  width: 23.3rem;
+`;
+
+const CountText = styled.p<{ $count: boolean; $percentage?: number }>`
+  display: flex;
   ${({ theme }) => theme.fonts.Caption_1};
 
-  color: ${({ theme, $count }) => ($count === true ? theme.colors.yellow_600 : theme.colors.gray_400)};
+  margin-left: ${({ $count, $percentage }) => ($count ? `${$percentage}%` : "45%")};
+  transform: translateX(-50%);
+  color: ${({ theme, $count }) => ($count ? theme.colors.yellow_600 : theme.colors.gray_400)};
+  text-align: center;
+`;
+
+const RewindIcon = styled(RewindIc)`
+  display: flex;
+  width: 7.6rem;
+  height: 7.6rem;
 `;
