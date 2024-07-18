7f8da68955a30aad34753218e3a70ef0fedbbb90
안혜연 <ahy020606@naver.com>
2024-07-19 02:22:46 +0900

    feat: 온보딩 입력 필드에 커서 포커스 추가

--- lubee/src/onboarding/code/index.tsx
+++ lubee/src/onboarding/code/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import OnboardingHeader from "../components/OnboardingHeader";
 import OnboardingTitleBox from "../components/OnboardingTitleBox";
@@ -13,6 +13,14 @@ interface CodeProps {
 export default function index(props: CodeProps) {
   const { moveToOnboardingConnect, moveToOnboardingCustom } = props;
   const [code, setCode] = useState("");
+  const yellowBoxRef = useRef<{ focus: () => void }>(null);
+
+  useEffect(() => {
+    // 페이지가 로드되고 나서 입력 필드에 포커스를 설정
+    if (yellowBoxRef.current) {
+      yellowBoxRef.current.focus();
+    }
+  }, []);
 
   const isOnboardingBtnDisabled = code === "";
 
@@ -29,7 +37,13 @@ export default function index(props: CodeProps) {
       <OnboardingHeader handleBackBtn={handleBackBtn} showBackIcon />
       <ContentsContainer>
         <OnboardingTitleBox titleText="연인의 러비코드를 입력하세요" />
-        <YellowBox inputValue={code} setInputValue={setCode} $disabled={true} placeholder="코드 입력" />
+        <YellowBox
+          inputValue={code}
+          setInputValue={setCode}
+          $disabled={true}
+          placeholder="코드 입력"
+          ref={yellowBoxRef}
+        />
       </ContentsContainer>
       <OnboardingBtn handleOnboardingBtn={handleOnboardingBtn} text="연결하기" $disabled={isOnboardingBtnDisabled} />
     </Wrapper>
