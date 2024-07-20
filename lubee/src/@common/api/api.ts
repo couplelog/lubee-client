3dc0da5e126669c5fc3588d188473f1cdb5ff544
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-20 11:41:49 +0900

    feat: api 헤더 content-type 추가

--- lubee/src/@common/api/api.ts
+++ lubee/src/@common/api/api.ts
@@ -2,6 +2,10 @@ import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
 
 export default api;
