099fe24dbcc38c09ee5ac1e4396691828856d9ad
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-20 17:35:49 +0900

    Update api.ts

--- lubee/src/@common/api/api.ts
+++ lubee/src/@common/api/api.ts
@@ -4,7 +4,6 @@ const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
   },
 });
 
