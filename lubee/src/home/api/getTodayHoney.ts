c89e0dd08fc4250d0252f668cdd8327e64e96b9c
안혜연 <ahy020606@naver.com>
2024-07-23 18:09:39 +0900

    feat: 오늘의 꿀 조회 API

---
+++ lubee/src/home/api/getTodayHoney.ts
@@ -0,0 +1,20 @@
+import api from "@common/api/api";
+import { Response } from "@common/types/Response";
+
+interface TodayHoneyDataTypes {
+  response: number;
+}
+
+// date 넘겨주기
+interface GetTodayHoneyRequest {
+  Date: Date;
+}
+
+// GET 요청에서는 보통 요청 바디를 사용하지 않음
+// 그래서 쿼리 파라미터로 전달
+export async function getTodayHoney(request: GetTodayHoneyRequest) {
+  const { data } = await api.get<Response<TodayHoneyDataTypes>>(`/api/datecomments/today`, {
+    params: { Date: request.Date },
+  });
+  return data;
+}
---
+++ lubee/src/home/hooks/useGetTodayHoney.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query";
+import { getTodayHoney } from "../api/getTodayHoney";
+
+export function useGetTodayHoney(Date: Date) {
+  const { data, error, isLoading } = useQuery(["getTodayDateComment", Date], () => getTodayHoney({ Date }), {
+    onError: (error) => {
+      // 서버에서 반환한 에러 메시지를 출력
+      if (error && (error as any).message) {
+        console.log("오류 발생:", (error as any).message);
+      } else {
+        console.log("알 수 없는 오류가 발생했습니다.");
+      }
+    },
+  });
+
+  return { data, error, isLoading };
+}
