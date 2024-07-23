194252aab1395e9990b0e9d626accdae3172a3ef
안혜연 <ahy020606@naver.com>
2024-07-23 19:39:26 +0900

    etc: 테스트 토큰 env에 저장

--- lubee/src/@common/api/api.ts
+++ lubee/src/@common/api/api.ts
@@ -4,8 +4,7 @@ const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzNjE3OTk4MjU1IiwiYXV0aCI6IlVTRVIiLCJleHAiOjE3MjE5MDMxODksImlhdCI6MTcyMTcyMzE4OX0.xKmyQrOVF84t_rwXthGVNtOMxYCRKZSqCAhtfoDTR0g",
+    Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
   },
 });
 
