19f32ac42efbd7c11c41da833165e6caa99412c0
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-23 19:39:22 +0900

    feat: 토큰 헤더에 추가

--- lubee/src/@common/api/api.ts
+++ lubee/src/@common/api/api.ts
@@ -4,6 +4,7 @@ const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
   },
 });
 
