edd88839b3230c1e68ce9e2bd7f9961cb5772312
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-25 21:33:28 +0900

    feat: delete 사진 url 수정

--- lubee/src/fullpic/api/deletePic.ts
+++ lubee/src/fullpic/api/deletePic.ts
@@ -1,7 +1,7 @@
 import api from "@common/api/api";
 
 export async function deletePic(memory_id: number) {
-  const { data } = await api.delete(`/api/memories/${memory_id}`);
+  const { data } = await api.delete(`/api/memories/?memory_id=${memory_id}`);
 
   return data;
 }
