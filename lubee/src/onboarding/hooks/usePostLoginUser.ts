e15763735c945793ce61e4bd6b928b28166d0b33
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-26 14:56:11 +0900

    etc: 온보딩 진행 postLoginUser API 경로 수정

--- lubee/src/onboarding/hooks/usePostLoginUser.ts
+++ lubee/src/onboarding/hooks/usePostLoginUser.ts
@@ -1,5 +1,5 @@
 import { useMutation } from "react-query";
-import { postLoginUser } from "login/api/postLoginUser";
+import { postLoginUser } from "onboarding/api/postLoginUser";
 
 export function usePostLoginUser() {
   return useMutation(postLoginUser, {
@@ -7,7 +7,7 @@ export function usePostLoginUser() {
       console.log("로그인한 유저가 맞고 커플이 맞다", data);
     },
     onError: (error) => {
-      console.log("코멘트 전송 실패", error);
+      console.log("사용자가 존재하지 않거나 해당 유저는 커플이 아니거나 기타에러", error);
     },
   });
 }
