d6f3758a6c75eef1e3e916313c980274ca14a1eb
안혜연 <ahy020606@naver.com>
2024-07-27 03:34:55 +0900

    feat: 생년월일을 setBirthday에 저장

--- lubee/src/onboarding/profile/index.tsx
+++ lubee/src/onboarding/profile/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import OnboardingHeader from "../components/OnboardingHeader";
