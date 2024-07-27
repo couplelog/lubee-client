1f432c5cedb6d6cff761e1bd89951da612e117e4
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-27 14:07:08 +0900

    feat: 축하페이지 버튼 조건 추가

--- lubee/src/congrats/index.tsx
+++ lubee/src/congrats/index.tsx
@@ -2,12 +2,21 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 import OnboardingBtn from "onboarding/components/OnboardingBtn";
+import { useGetCouplesInfo } from "@common/hooks/useGetCouplesInfo";
+import { infoToast } from "@common/utils/toast";
 
 export default function index() {
   const navigate = useNavigate();
+  const couplesInfoResponse = useGetCouplesInfo();
+  console.log("커플정보를 얻을 수 있는지 확인 ", couplesInfoResponse?.success);
 
   function handleOnboardingBtn() {
-    navigate("/home/today");
+    if (couplesInfoResponse?.success) {
+      // 둘다 온보딩 post를 날려서 커플정보를 얻을 수 있는 경우
+      navigate("/home/today");
+    } else {
+      infoToast("연인이 커플정보를 입력하지 않았어요!");
+    }
   }
 
   return (
