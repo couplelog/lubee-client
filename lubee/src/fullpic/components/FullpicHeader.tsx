9b117ec784e1d72025c2747cc70fa9bbc12d9a48
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-27 11:44:26 +0900

    feat: Fullpic 헤더 뒤로가기시 이모지 post, delete

--- lubee/src/fullpic/components/FullpicHeader.tsx
+++ lubee/src/fullpic/components/FullpicHeader.tsx
@@ -1,18 +1,32 @@
 import { BackIc, TrashIc } from "@assets/index";
+import { useDeleteReaction } from "@common/hooks/useDeleteReaction";
+import { usePostReaction } from "@common/hooks/usePostReaction";
 import { BtnWrapper } from "@styles/btnStyle";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 interface FullpicHeaderProps {
   handleTrashBtn: (open: boolean) => void;
   headerDate: string;
+  selectedEmojiText: string;
+  memory_id: number;
 }
+
 export default function FullpicHeader(props: FullpicHeaderProps) {
-  const { handleTrashBtn, headerDate } = props;
+  const { handleTrashBtn, headerDate, selectedEmojiText, memory_id } = props;
   const navigate = useNavigate();
+
+  /*뒤로가기하면 반응이 안바뀌더라도 무조건 deleteReaction+postReaction */
+  const { mutate: postReactionMutate } = usePostReaction();
+  const { mutate: deleteReactionMutate } = useDeleteReaction();
 
   function moveToHome() {
     // 헤더에서 전에 어떤 페이지였는지 불러오기
     const prevPage = localStorage.getItem("currentPage");
+
+    // 기존 리액션 delete
+    deleteReactionMutate(memory_id);
+    // 새로운 리액션 post
+    postReactionMutate({ memory_id: memory_id, reaction: selectedEmojiText });
 
     if (prevPage === "today") {
       navigate("/home/today");
