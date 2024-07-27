2347a335f185f9bc0fd0a58c1aab590bb4a9b6db
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-27 17:38:50 +0900

    feat: loading 페이지

--- lubee/src/loading/index.tsx
+++ lubee/src/loading/index.tsx
@@ -1,3 +1,64 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { LogoGrayIc } from "assets/index";
+import CompanyText from "@common/components/CompanyText";
+
 export default function index() {
-  return <div>Loading중...</div>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    localStorage.getItem("currentPage");
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const prevPage = localStorage.getItem("currentPage");
+      if (prevPage === "today") {
+        navigate("/home/today");
+      } else {
+        navigate("/home/month");
+      }
+    }, 3000); // 3초 후에 로그인 페이지로 이동
+
+    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 정리
+  }, [navigate]);
+
+  return (
+    <Wrapper>
+      <LogoContainer>
+        <Text>loading...</Text>
+        <LogoIcon />
+      </LogoContainer>
+      <CompanyText />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.yellow};
+`;
+
+const LogoContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  align-items: center;
+  margin-top: 27.8rem;
+`;
+
+const Text = styled.p`
+  ${({ theme }) => theme.fonts.Body_0};
+
+  color: ${({ theme }) => theme.colors.gray_700};
+`;
+
+const LogoIcon = styled(LogoGrayIc)`
+  width: 15rem;
+  height: 3.9rem;
+`;
