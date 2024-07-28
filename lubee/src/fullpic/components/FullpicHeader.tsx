0317badb8a415e93673670581c4fc560ea2b88d5
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-28 13:59:45 +0900

    feat: FullPicHeader에 현재 저장된 이모지는 무엇인지 불러오기

--- lubee/src/fullpic/components/FullpicHeader.tsx
+++ lubee/src/fullpic/components/FullpicHeader.tsx
@@ -1,39 +1,68 @@
 import { BackIc, TrashIc } from "assets/index";
-import { useDeleteReaction } from "@common/hooks/useDeleteReaction";
+import { useUpdateReaction } from "@common/hooks/useUpdateReaction";
 import { usePostReaction } from "@common/hooks/usePostReaction";
 import { BtnWrapper } from "@styles/btnStyle";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+
 interface FullpicHeaderProps {
   handleTrashBtn: (open: boolean) => void;
   headerDate: string;
   selectedEmojiText: string;
   memory_id: number;
+  reaction_first: string | null;
 }
 
 export default function FullpicHeader(props: FullpicHeaderProps) {
-  const { handleTrashBtn, headerDate, selectedEmojiText, memory_id } = props;
+  const { handleTrashBtn, headerDate, selectedEmojiText, memory_id, reaction_first } = props;
   const navigate = useNavigate();
 
   /*뒤로가기하면 반응이 안바뀌더라도 무조건 deleteReaction+postReaction */
   const { mutate: postReactionMutate } = usePostReaction();
-  const { mutate: deleteReactionMutate } = useDeleteReaction();
+  const { mutate: updateReactionMutate } = useUpdateReaction();
+
+  console.log("myEmoji", reaction_first);
 
   function moveToHome() {
-    // 헤더에서 전에 어떤 페이지였는지 불러오기
     const prevPage = localStorage.getItem("currentPage");
 
-    // 기존 리액션 delete
-    deleteReactionMutate(memory_id);
-    // 새로운 리액션 post
-    postReactionMutate({ memory_id: memory_id, reaction: selectedEmojiText });
-
-    if (prevPage === "today") {
-      navigate("/home/today");
-      console.log(prevPage);
-    } else {
-      navigate("/home/month");
-      console.log(prevPage);
+    // 새로운 리액션 추가
+    if (reaction_first === null && selectedEmojiText !== null) {
+      postReactionMutate(
+        { memory_id: memory_id, reaction: selectedEmojiText },
+        {
+          onSuccess: () => {
+            if (prevPage === "today") {
+              navigate("/home/today");
+            } else {
+              navigate("/home/month");
+            }
+          },
+        },
+      );
+    }
+    // 리액션이 다를때
+    else if (reaction_first !== selectedEmojiText) {
+      updateReactionMutate(
+        { memory_id: memory_id, reaction: selectedEmojiText },
+        {
+          onSuccess: () => {
+            if (prevPage === "today") {
+              navigate("/home/today");
+            } else {
+              navigate("/home/month");
+            }
+          },
+        },
+      );
+    }
+    // no change일때
+    else {
+      if (prevPage === "today") {
+        navigate("/home/today");
+      } else {
+        navigate("/home/month");
+      }
     }
   }
 
