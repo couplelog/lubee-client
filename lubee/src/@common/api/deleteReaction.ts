920f8561acc2385450cfcfc7f9b069d5bdf174ff
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-28 13:58:30 +0900

    feat: delete 통신 삭제

--- lubee/src/@common/api/deleteReaction.ts
+++
@@ -1,7 +0,0 @@
-import api from "@common/api/api";
-
-export async function deleteReaction(memory_id: number) {
-  const { data } = await api.delete(`/api/memories/reaction/?memory_id=${memory_id}`);
-
-  return data;
-}
