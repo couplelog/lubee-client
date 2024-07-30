37b5079d936f7ac08667e27a8b496d649520f4b9
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-30 15:59:24 +0900

    revert login

--- lubee/src/@common/hooks/useGetCouplesInfo.ts
+++ lubee/src/@common/hooks/useGetCouplesInfo.ts
@@ -1,10 +1,8 @@
 import { useQuery } from "react-query";
 import { getCouplesInfo } from "@common/api/getCouplesInfo";
 
-// isLoggedIn 기본값을 true로 설정
-export function useGetCouplesInfo(isLoggedIn: boolean = true) {
+export function useGetCouplesInfo() {
   const { data, isLoading, error } = useQuery("getCouplesInfo", getCouplesInfo, {
-    enabled: isLoggedIn, // isLoggedIn이 true일 때만 쿼리 실행
     onError: (error) => {
       console.log("에러 발생", error);
     },
