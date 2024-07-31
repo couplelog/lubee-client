7ceffc61c99df27cc7e24b93d8cedeee1d7c3c4c
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-31 10:54:54 +0900

    etc: 코멘트 확인 console 추가

--- lubee/src/home/today/components/ContentContainer.tsx
+++ lubee/src/home/today/components/ContentContainer.tsx
@@ -44,6 +44,11 @@ export default function ContentContainer(props: ContentContainerProps) {
   const myComment = response?.comment_first || "";
   const partnerComment = response?.comment_second || "";
 
+  useEffect(() => {
+    console.log("내 코멘트", myComment);
+    console.log("partner코멘트", partnerComment);
+  }, [response, commentData]);
+
   // 데이터가 없을 경우 빈 화면을 반환
   if (!CoupleInfo) return <></>;
 
