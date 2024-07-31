8534ae862844bfec0227d577110b05404b318dd1
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-31 21:42:04 +0900

    errorToast("오늘 이전의 날짜를 선택해주세요");

--- lubee/src/home/month/components/CalContainer.tsx
+++ lubee/src/home/month/components/CalContainer.tsx
@@ -4,7 +4,7 @@ import { HoneyMonthIc } from "assets/index";
 import DateDetailModal from "./DateDetailModal";
 import { useEffect, useRef, useState } from "react";
 import { formatMonth, getTodayDate, getTodayMonth, getTodayYear, isFutureDate } from "@common/utils/dateFormat";
-import { infoToast } from "@common/utils/toast";
+import { errorToast } from "@common/utils/toast";
 import { useGetCalendar } from "home/hooks/useGetCalendar";
 import { useGetMonthHoney } from "home/hooks/useGetMonthHoney";
 
@@ -68,7 +68,7 @@ const CalContainer = ({ info, showCalendar = false, setOpenDateDetailModal }: Ca
 
   function handleDateDetailModal(date: number) {
     if (isFutureDate(year, month, date)) {
-      infoToast("오늘 이전의 날짜를 선택해주세요");
+      errorToast("오늘 이전의 날짜를 선택해주세요");
       setSelectedDate(date);
       return;
     }
