dd008b5488b8aa19ab212770949380f24eb651f9
Rose <120544840+Rose-my@users.noreply.github.com>
2024-07-31 22:43:02 +0900

    useGetCouplesInfo도 interval

--- lubee/src/@common/hooks/useGetCouplesInfo.ts
+++ lubee/src/@common/hooks/useGetCouplesInfo.ts
@@ -3,11 +3,12 @@ import { getCouplesInfo } from "@common/api/getCouplesInfo";
 
 // isLoggedIn 기본값을 true로 설정
 export function useGetCouplesInfo(isLoggedIn: boolean = true) {
-  const { data, isLoading, error } = useQuery("getCouplesInfo", getCouplesInfo, {
+  const { data, isLoading, error, refetch } = useQuery("getCouplesInfo", getCouplesInfo, {
     enabled: isLoggedIn, // isLoggedIn이 true일 때만 쿼리 실행
+    refetchInterval: 3000, // 3초마다 데이터를 refetch
     onError: () => {
       console.log("커플정보가져오기 에러", error);
     },
   });
-  return { data, isLoading, error };
+  return { data, isLoading, error, refetch };
 }
