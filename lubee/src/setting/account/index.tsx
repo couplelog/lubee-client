c7d31c1ca053a6b2d0e56fd92803f56b6657b581
안혜연 <ahy020606@naver.com>
2024-08-12 14:01:40 +0900

    design: SettingHeader 공통 컴포넌트

--- lubee/src/setting/account/index.tsx
+++ lubee/src/setting/account/index.tsx
@@ -1,5 +1,12 @@
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import SettingHeader from "../components/SettingHeader";
 
 export default function index() {
-  return <div>index</div>;
+  const navigate = useNavigate();
+
+  function handleBackBtn() {
+    navigate("/mypage");
+  }
+
+  return <SettingHeader handleBackBtn={handleBackBtn} title="환경 설정" />;
 }
--- lubee/src/setting/couple/index.tsx
+++ lubee/src/setting/couple/index.tsx
@@ -1,5 +1,12 @@
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import SettingHeader from "../components/SettingHeader";
 
 export default function index() {
-  return <div>index</div>;
+  const navigate = useNavigate();
+
+  function handleBackBtn() {
+    navigate("setting/account");
+  }
+
+  return <SettingHeader handleBackBtn={handleBackBtn} title="커플 연결" />;
 }
