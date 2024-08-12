69e1abf2ccfb32d2306b7b0c7a6488315e96b23b
안혜연 <ahy020606@naver.com>
2024-08-12 15:42:00 +0900

    design: 환경 설정 페이지

--- lubee/src/setting/account/index.tsx
+++ lubee/src/setting/account/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import SettingHeader from "../components/SettingHeader";
@@ -6,6 +7,7 @@ import { SettingRightArrowIc } from "assets/index";
 
 export default function index() {
   const navigate = useNavigate();
+  const [isModalOpen, setModalOpen] = useState(false);
 
   function handleBackBtn() {
     navigate("/mypage");
@@ -16,22 +18,45 @@ export default function index() {
   }
 
   function handleCloseBtn() {
+    setModalOpen(false); // 모달 닫기
     navigate("/setting/account");
   }
 
   return (
     <Wrapper>
       <SettingHeader handleBackBtn={handleBackBtn} title="환경 설정" />
-      <TextContainer>
+      <ContentContainer>
         <TitleText>개인 설정</TitleText>
-      </TextContainer>
-      <ConfirmModal
-        handleConfirmBtn={handleConfirmBtn}
-        handleCloseBtn={handleCloseBtn}
-        titleText="정말로 탈퇴하시겠어요?"
-        subtitleText="영구적으로 계정이 삭제됩니다"
-        btnText="탈퇴"
-      />
+        <BtnWrapper
+          type="button"
+          onClick={() => {
+            navigate("/setting/couple");
+          }}>
+          <MenuContainer>
+            <SubtitleText>커플 연결</SubtitleText>
+            <RightArrowIcon />
+          </MenuContainer>
+        </BtnWrapper>
+        <BtnWrapper
+          type="button"
+          onClick={() => {
+            setModalOpen(true);
+          }}>
+          <MenuContainer>
+            <SubtitleText>회원 탈퇴</SubtitleText>
+            <RightArrowIcon />
+          </MenuContainer>
+        </BtnWrapper>
+      </ContentContainer>
+      {isModalOpen && (
+        <ConfirmModal
+          handleConfirmBtn={handleConfirmBtn}
+          handleCloseBtn={handleCloseBtn}
+          titleText="정말로 탈퇴하시겠어요?"
+          subtitleText="영구적으로 계정이 삭제됩니다"
+          btnText="탈퇴"
+        />
+      )}
     </Wrapper>
   );
 }
@@ -39,23 +64,38 @@ export default function index() {
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  position: relative;
   width: 100%;
   height: 100%;
 `;
 
-const TextContainer = styled.section`
+const ContentContainer = styled.section`
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
-  align-items: center;
+  align-items: flex-start;
+  width: 100%;
+  padding: 1.6rem 2rem;
 `;
 
 const TitleText = styled.h2`
   ${({ theme }) => theme.fonts.Body_4};
 
   color: ${({ theme }) => theme.colors.gray_800};
+`;
+
+const BtnWrapper = styled.button`
+  width: 100%;
+  padding: 0;
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+
+const MenuContainer = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `;
 
 const SubtitleText = styled.h2`
