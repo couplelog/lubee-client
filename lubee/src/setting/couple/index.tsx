bdb41ac46a17f84b37db308ac303c0f13c8dcf90
안혜연 <ahy020606@naver.com>
2024-08-14 18:55:08 +0900

    design: setting 페이지 Wrapper 수정

--- lubee/src/setting/couple/index.tsx
+++ lubee/src/setting/couple/index.tsx
@@ -74,9 +74,6 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
-  width: 100%;
-  height: 100%;
 `;
 
 const ContentContainer = styled.section`
